2b4541888747fe67976baa44ef5b091cc63d3b15
Laevahteinn <cade.haynes@gmail.com>
2026-03-12 18:30:34 +1100

    Updated Ability and Item DTOs to not store IDs

--- DTOs/ItemDto.cs
+++ DTOs/ItemDto.cs
@@ -3,7 +3,6 @@
     public class ItemDto
     {
         // Item Info
-        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
     }
