febf184c1ba24f27a24c6ff8a7437108e36e277a
Laevahteinn <cade.haynes@gmail.com>
2026-03-31 23:34:04 +1100

    ItemController now has Get for all Items for a character, also ItemGetDto is used for this.

--- Controllers/ItemController.cs
+++ Controllers/ItemController.cs
@@ -1,7 +1,32 @@
-﻿namespace DnDCharacterManager.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using DnDCharacterManager.Data;
+using DnDCharacterManager.DTOs;
+
+namespace DnDCharacterManager.Controllers
 {
-    public class ItemController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ItemController : ControllerBase
     {
-        //
+        readonly AppDbContext _context;
+
+        public ItemController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("character/{characterId}")]
+        public async Task<ActionResult<IEnumerable<ItemGetDto>>> GetItemsForCharacter(int characterId)
+        {
+            var items = await _context.Items
+                .Where(i => i.CharacterId == characterId)
+                .ToListAsync();
+
+            var itemDtos = items.Select(i => ItemGetDto.FromItem(i)).ToList();
+
+            return itemDtos;
+        }
     }
 }
---
+++ DTOs/Item/ItemGetDto.cs
@@ -0,0 +1,23 @@
+﻿using DnDCharacterManager.Models;
+
+namespace DnDCharacterManager.DTOs
+{
+    public class ItemGetDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public static ItemGetDto FromItem(Item item)
+        {
+            var dto = new ItemGetDto
+            {
+                Id = item.Id,
+                Name = item.Name,
+                Description = item.Description
+            };
+
+            return dto;
+        }
+    }
+}
